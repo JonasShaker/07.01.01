fd1136161ffe3f7b6912013e7b902bb23bcb641d
Jonas <joab0001@stud.kea.dk>
2023-08-31 17:43:55 +0200

    commit

--- java/product.js
+++ java/product.js
@@ -11,17 +11,32 @@ function showProduct(product){
     console.log(product);
     document.querySelector(".purchaseBox h3").textContent=
     product.productdisplyname;
-    document.querySelector(".purchaseBox .brand").textContent = product.brandname;
-    document.querySelector(
-        "img"
-    ). src= `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
+    if (!product.soldout) {
+        // prodyktet er udsolgt 
+        console.log("udsolgt")
+        document.querySelector(".soldout").remove();
+    }
+    if (!product.discount){
+        // prodyktet er på udsalg 
+        console.log("på udsalg ")
+    } else{
+        document.querySelector(".discounted").remove();
+    }
 
-    document.querySelector(".InventoryNumber").textContent = product.relid;
-    document.querySelector(".ModelName").textContent = product.productdisplayname;
+    // document.querySelector(".InventoryNumber").textContent = product.relid;
+
     document.querySelector(".color").textContent = product.basecolour;
-    document.querySelector(".Name").textContent = product.productdisplayname;
-    document.querySelector(".variantname").textContent = product.variantname;
+    // document.querySelector(".Name").textContent = product.productdisplayname;
+    document.querySelector(".produkt").textContent = product.variantname;
     document.querySelector(".brandbio").textContent = product.brandbio;
     document.querySelector(".season").textContent = product.season;
     document.querySelector(".Gender").textContent = product.gender;
+    document.querySelector(".brand").textContent = product.articletype;
+    document.querySelector(".price").textContent = product.price;
+    document.querySelector(".purchaseBox .brand").textContent = product.brandname;
+    document.querySelector("img"). src= `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
+
+
+
+
 }
