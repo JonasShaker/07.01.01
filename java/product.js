293eba2f70de4b697a4ad76b32a90349004724d9
Jonas <joab0001@stud.kea.dk>
2023-08-31 09:39:40 +0200

    produkt

--- java/product.js
+++ java/product.js
@@ -16,5 +16,12 @@ function showProduct(product){
         "img"
     ). src= `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`
 
-
+    document.querySelector(".InventoryNumber").textContent = product.relid;
+    document.querySelector(".ModelName").textContent = product.productdisplayname;
+    document.querySelector(".color").textContent = product.basecolour;
+    document.querySelector(".Name").textContent = product.productdisplayname;
+    document.querySelector(".variantname").textContent = product.variantname;
+    document.querySelector(".brandbio").textContent = product.brandbio;
+    document.querySelector(".season").textContent = product.season;
+    document.querySelector(".Gender").textContent = product.gender;
 }
